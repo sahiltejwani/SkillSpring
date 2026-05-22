585debe992393172729fb19c9ba4bf21084297fb
sahiltejwani <tejwanisahil73@gmail.com>
2026-05-22 17:06:11 +0530

    try to fix it

--- Server/utils/mailSender.js
+++ Server/utils/mailSender.js
@@ -3,11 +3,12 @@ const nodemailer = require("nodemailer");
 const mailSender = async (email, title, body) => {
     try{
         let transporter = nodemailer.createTransport({
-            host: process.env.MAIL_HOST,
+            // host: process.env.MAIL_HOST,
+            service: "gmail",
             auth: {
                 user: process.env.MAIL_USER,
                 pass: process.env.MAIL_PASS,
-            }
+            }   
         });
 
         console.log("Transporter Created Successfully");
